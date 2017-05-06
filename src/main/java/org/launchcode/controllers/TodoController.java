1ecabceb7d39e37a80c688a2a546c0404979bf45
Jeannie Fallon <jeanniefallon@gmail.com>
2017-05-05 21:00:51 -0400

    Add post method to TodoController

--- src/main/java/org/launchcode/controllers/TodoController.java
+++ src/main/java/org/launchcode/controllers/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by jeannie on 5/2/17.
@@ -20,12 +21,23 @@ public class TodoController {
     @Autowired
     private TaskDao taskDao;
 
-    @RequestMapping(value = "")
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("title","Current Tasks");
         model.addAttribute("tasks", taskDao.findAll());
         return "todo/index";
     }
+
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String index(@RequestParam int[] taskIds) {
+        for (int taskId : taskIds) {
+            taskDao.delete(taskId);
+        }
+        return "redirect:todo";
+    }
+
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
@@ -33,6 +45,7 @@ public class TodoController {
         model.addAttribute("task", new Task());
         return "todo/add";
     }
+
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String add(@ModelAttribute Task task, Model model) {
@@ -46,4 +59,5 @@ public class TodoController {
         model.addAttribute("title", "About");
         return "todo/about";
     }
+
 }
